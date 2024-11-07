f20eab0f34b5dd5de8f8fbd43dce10e2b1f1820c
Ludmila Almeida <ludalmeidaa03@gmail.com>
2024-11-07 18:13:05 -0300

    1commit
    Adicionando o inicio do projeto

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '12345',
+    database: process.env.DB_NAME || 'careacademy',
+    port: process.env.DB_PORT || 3306
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+        return;
+    }
+    console.log('Conectado ao banco de dados MySQL!');
+});
